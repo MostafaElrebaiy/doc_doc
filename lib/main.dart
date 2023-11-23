3e459d9a90e342806f15442554c965eaae4f0458
Mostafaibrahem <darshmostafa690@gmail.com>
2023-11-23 21:33:27 +0200

    launching project

---
+++ lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_new_project/core/routing/app_router.dart';
+import 'package:flutter_new_project/doc_app.dart';
+
+void main() {
+  runApp( DocApp(appRouter: AppRouter(),));
+}
+
+
